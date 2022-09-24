952be4b1bd40fba15408ddf9a2a50c897096bf63
Olga <politykoolga@outlook.com>
2022-09-24 15:13:33 +0300

    First commit

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿int a = 9;
+int b = 9;
+int[,] array = new int[a, b];
+Random rnd = new Random();
+int numsum = Int32.MaxValue;
+int index = 0;
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.Write($"\n");
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < a; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < b; j++)
+        {
+            sum = sum + array[i, j];
+        }
+        if (sum < numsum)
+        {
+            numsum = sum;
+            index++;
+        }
+    }
+    Console.WriteLine("Строка с наименьшей суммой: " + (index) + ", сумма равна: " + (numsum));
+}
+
+PrintArray(array);
+FillArray(array);
